6dc95de0eba459b1005a59ed69f2a9733c4181fb
unknown <flowerrosa76@gmail.com>
2023-08-28 14:52:47 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+
+// const btn = document.querySelector(".btn");
+
+// btn.addEventListener("click", function () {
+//   gsap.to(".btn", 1, {
+//     opacity: 0,
+//     y: -40,
+//     ease: Expo.easeInOut,
+//   });
+
+//   gsap.to(".text-wrapper > div", 1, {
+//     x: "500",
+//     ease: Expo.easeInOut,
+//     delay: 1,
+//     stagger: 0.1,
+//   });
+
+//   gsap.to(".text-wrapper", 3, {
+//     y: -600,
+//     scale: 4.5,
+//     rotate: -90,
+//     ease: Expo.easeInOut,
+//     delay: 1.5,
+//   });
+
+//   gsap.to(".text", 1, {
+//     opacity: 1,
+//     ease: Expo.easeInOut,
+//     delay: 3,
+//   });
+
+//   gsap.to(".text-wrapper > div", 4, {
+//     x: "-3500",
+//     ease: Expo.easeInOut,
+//     delay: 2.5,
+//     stagger: 0.05,
+//   });
+
+//   gsap.to(".text-container", 2, {
+//     bottom: "-100%",
+//     ease: Expo.easeInOut,
+//     delay: 5,
+//   });
+
+ 
+//   anime.timeline().add({
+//     targets: ".header .letter",
+//     opacity: [0, 1],
+//     translateY: [200, 0],
+//     translateZ: 0,
+//     easing: "easeOutExpo",
+//     duration: 2000,
+//     delay: (el, i) => 7000 + 40 * i,
+//   });
+
+
+//  setTimeout(function(){
+//   window.location.href="main.html"},9000);
+
+// });
+
+setTimeout(function(){
+  window.location.href="main.html"
+},6000);
